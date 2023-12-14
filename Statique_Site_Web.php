791928b8f0f04ebaa9d8e86ca6437ea69ac7cea3
byllstephane <agbebyll@gmail.com>
2023-12-14 00:19:44 -0500

    Ajout du fichier1-Statique_Site_Web

---
+++ Statique_Site_Web.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>WELCOME ON MY SITE</title>
+    </head>
+
+    <body>
+        <h2>La Force Est Avec Toi</h2>
+        <p>
+            Cette page contient <strong>uniquement</strong> des codes .<br/>
+            <?php echo("My First Application."); ?>
+
+
+            Voici quelques petits tests :
+
+
+        </p>
+
+        <ul>
+            <li style="color: blue;">Texte en bleu</li>
+            <li style="color: red;">Texte en rouge</li>
+            <li style="color: green;">Texte en vert</li>
+        </ul>
+        <?php
+        echo "Je suis ouvert.";
+        /* Encore du PHP
+        Toujours du PHP */
+        ?>
+    </body>
+</html>
